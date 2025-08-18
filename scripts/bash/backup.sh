ed0388153816fff0a7046a0df5829bb7f239e475
RootCellar <RootCellar9877@gmail.com>
2025-08-17 17:43:15 -0800

    add initial backup script with some improvements

---
+++ scripts/bash/backup.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# Inputs:
+# RSYNC_BASE (optional)
+# LOG_FILE
+# RSYNC_OPTIONS
+
+RSYNC_BASE=${RSYNC_BASE:-"rsync"}
+
+source scripts/bash/date.sh
+
+function log() {
+  echo "$*" >> "${LOG_FILE}"
+}
+
+function backup() {
+  if [ -z "${LOG_FILE}" ]; then
+    echo "Refusing to run with no LOG_FILE given."
+    return 1
+  fi
+
+  if [ -z "${RSYNC_OPTIONS}" ]; then
+    echo "Refusing to run with no RSYNC_OPTIONS given."
+    return 1
+  fi
+
+  log Backing up '"'"$1"'"' to '"'"$2"'"'...
+
+  command_to_print="${RSYNC_BASE} ${RSYNC_OPTIONS[*]} $*"
+
+  log
+  log "Command: ${command_to_print} "
+  log "Sync Begin: $(date_and_time) "
+  log
+
+  ${RSYNC_BASE} "${RSYNC_OPTIONS[@]}" "$@" >> "${LOG_FILE}" 2>&1 || {
+    echo "Error encountered during backup. See ${LOG_FILE}"
+    return 1
+  }
+  log
+  log "Sync End: $(date_and_time) "
+  return 0
+}
+
